3a682c702dd436bad461a7511dbb2f197e03adf2
KTOyaAmojetTI <vlad.rakovich@yandex.ru>
2020-04-18 00:14:15 +0600

    delete news

---
+++ news.php
@@ -0,0 +1,22 @@
+<?php
+require($_SERVER["DOCUMENT_ROOT"]."/models/DB.php");
+require($_SERVER["DOCUMENT_ROOT"]."/controllers/news.php");
+$newsList=news::getList();
+
+if ($_GET["id"] != 0):
+    news::delete_news($_GET["id"]);
+endif;
+?>
+
+<table>
+    <tr><td>ID</td><td>Название новости</td><td>Действия</td></tr>
+    <?php foreach($newsList as $arNews):
+        $arr[] = $arNews['id']?>
+        <tr>
+            <td><?=$arNews["id"];?></td>
+            <td><?=$arNews["name"];?></td>
+            <td><a href="?id=<?=$arNews["id"];?>">Удалить</a><br/>
+            </td>
+        </tr>
+    <?php endforeach;?>
+</table>
