ccd26262c09a1dbf8a076f0c194d50b5a907ec48
Samoylov <kst.sm@yandex.ru>
2020-04-18 20:01:03 +0600

    123

---
+++ public/controllers/news.php
@@ -0,0 +1,17 @@
+<?php
+class news
+{
+    Public static function getList(){
+        $all = DB::run("SELECT id, name FROM news")->fetchAll();
+        return $all;
+    }
+
+    public static function actionDelete($id){
+        $del = DB::run("DELETE FROM news WHERE id=?", array($id));
+    }
+
+    Public static function getByID($id){
+        $row = DB::run("SELECT * FROM news WHERE id=?", array($id))->fetch();
+        return $row ;
+    }
+}
---
+++ public/news.sql
@@ -0,0 +1,24 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE DATABASE IF NOT EXISTS `data` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `data`;
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `news` (`id`, `name`) VALUES
+(1, 'date'),
+(2, 'send');
+
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
