070027df795d00758f9c6c7c8b8dacfb3683e0e9
Fokus <Maksimka2142@yandex.ru>
2020-04-21 13:34:29 +0600

    new file for edit news (Form)

--- controllers/news.php
+++ controllers/news.php
@@ -1,19 +1,26 @@
 <?php
 class news
 {
-/* 
-получаем полного списка новостей, лучше сделать ограничение и использовать в запросе LIMIT и выводить ни весь список, а  постранично
-*/
-   Public static function getList(){
-$all = DB::run("SELECT id, name FROM news")->fetchAll();
-return $all;
-	}
-/*
- Получаем конкретную новость по ее id
-*/
-   Public static function getByID($id){
-$row = DB::run("SELECT * FROM news WHERE id=?", array($id))->fetch();
-return $row ;
-	}
-
+    Public static function getList(){
+        $all = DB::run("SELECT id, name FROM news")->fetchAll();
+        return $all;
+    }
+    public static function delete_news($id){
+        $del = DB::run("DELETE FROM news WHERE id=?", array($id));
+    }
+    Public static function getByID($id){
+        $row = DB::run("SELECT * FROM news WHERE id=?", array($id))->fetch();
+        return $row ;
+    }
+    Public static function update ($id, $data=array())
+    {
+        $str="";
+        Foreach($data as $key=>$val)
+        {
+          $str.= $key."=:".$key;
+        }
+    $data["id"]=$id;
+    $result = DB::run ("Update news SET".$str."where id=:id", array($data));
+    return $result;
+    }
 }
