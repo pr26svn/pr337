9bd32943f43ec5d8a95de2ba6d4c0401295af31e
Fokus <Maksimka2142@yandex.ru>
2020-04-21 13:38:47 +0600

    file with connect to Data Base

--- news.php
+++ news.php
@@ -1,19 +1,32 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Таблица Новости</title>
+	<link rel="stylesheet" href=style.css>
+</head>
+<body>
 <?php
 require($_SERVER["DOCUMENT_ROOT"]."/models/DB.php");
 require($_SERVER["DOCUMENT_ROOT"]."/controllers/news.php");
 $newsList=news::getList();
+
+if ($_GET["id"] != 0):
+    news::delete_news($_GET["id"]);
+endif;
 ?>
 
 <table>
-<tr><td>ID</td><td>Название новости</td><td>Действия</td></tr>
-<?php foreach($newsList as $arNews):?>
-		<tr>
-<td><?=$arNews["id"];?></td>
-<td><?=$arNews["name"];?></td>
-<td>
-<a href="/news.php?id=<?=$arNews["id"];?>">Удалить</a><br/>
-<a href="/editNews.php?id=<?=$arNews["id"];?>">Изменить</a>
-</td>
-</tr>
-<?php endforeach;?>
+    <tr><td>ID</td><td>Название новости</td><td>Действия</td></tr>
+    <?php foreach($newsList as $arNews):
+        $arr[] = $arNews['id']?>
+        <tr>
+            <td><?=$arNews["id"];?></td>
+            <td><?=$arNews["name"];?></td>
+            <td><a href="?id=<?=$arNews["id"];?>">Удалить</a><br/>
+                <a href="/editNews.php?id=<?=$arNews["id"];?>">Изменить</a>
+            </td>
+        </tr>
+    <?php endforeach;?>
 </table>
+</html>
